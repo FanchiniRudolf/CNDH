c7df1e88d3522be279f5bc7dac69d1280b7ed0ae
FanchiniRudolf <43999588+FanchiniRudolf@users.noreply.github.com>
2019-06-22 11:01:12 -0500

    nombre video

--- videoCMD.py
+++ videoCMD.py
@@ -1,5 +1,5 @@
 import subprocess
 
 
-comando = 'vlc --play-and-exit --no-video-deco -f assetsCNDH\\prueba.mp4'
+comando = 'vlc --play-and-exit --no-video-deco -f assetsCNDH\\directorio.mp4'
 subprocess.run(comando, shell=True)
