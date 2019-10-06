1622ca46eaccdf229912468415c55c974efe51fa
FanchiniRudolf <fanchinirudolf@gmail.com>
2019-10-06 12:55:20 -0500

    second building init

---
+++ TorreAB/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
+
+//init win
+let win;
+
+function createWindow(){
+   win = new BrowserWindow({fullscreen:true});
+
+     // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes:true
+
+    }));
+
+   //closed event
+   win.on("closed", () =>{
+       win = null;
+   });
+   win.setMenu(null);
+
+   //win.webContents.openDevTools();
+}
+
+//run createWin
+app.on("ready", createWindow);
+
+//close when all windows are close
+app.on("window-all-closed", app.quit);
